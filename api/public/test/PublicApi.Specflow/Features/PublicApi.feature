dc49e0fce8da2c5b801499cc1c62db73e442f046
yvzn <mail@ludeo.net>
2018-12-20 13:35:02 +0100

    [CT-53] Mise en place des scenarios SpecFlow pour l'API publique

---
+++ api/public/test/PublicApi.Specflow/Features/PublicApi.feature
@@ -0,0 +1,27 @@
+﻿Feature: Back-end API for user-mgmt
+
+@ignore
+Scenario: S_1
+Given an Employee with valid credentials
+When I login as this Employee
+Then I can access the application according to my access rights
+
+@ignore
+Scenario: S_2
+Given an Employee list
+When I bulk insert this list
+Then all employees are created or updated
+
+@ignore
+Scenario: S_3
+Given an Employee
+And an authenticated external application
+When that external application requests for personal data of this Employee
+Then it can read personal data of this Employee
+
+@ignore
+Scenario: S_4
+Given an Employee
+And an authenticated external application
+When this employee is updated
+Then that external application is notified of the update
---
+++ api/public/test/PublicApi.Specflow/StepDefinitions/PublicApiSteps.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace PublicApi.SpecFlow.StepDefinitions
+{
+    [Binding]
+    public class PublicApiSteps
+    {
+        [Given(@"an Employee with valid credentials")]
+        public void GivenAnEmployeeWithValidCredentials()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"an Employee list")]
+        public void GivenAnEmployeeList()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"an Employee")]
+        public void GivenAnEmployee()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"an authenticated external application")]
+        public void GivenAnAuthenticatedExternalApplication()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I login as this Employee")]
+        public void WhenILoginAsThisEmployee()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I bulk insert this list")]
+        public void WhenIBulkInsertThisList()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"that external application requests for personal data of this Employee")]
+        public void WhenThatExternalApplicationRequestsForPersonalDataOfThisEmployee()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"this employee is updated")]
+        public void WhenThisEmployeeIsUpdated()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I can access the application according to my access rights")]
+        public void ThenICanAccessTheApplicationAccordingToMyAccessRights()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"all employees are created or updated")]
+        public void ThenAllEmployeesAreCreatedOrUpdated()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"it can read personal data of this Employee")]
+        public void ThenItCanReadPersonalDataOfThisEmployee()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"that external application is notified of the update")]
+        public void ThenThatExternalApplicationIsNotifiedOfTheUpdate()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
